4badbed8379795f1a651ff75eeeaf0faa912b5fc
Wishmaster01 <Wishmaster01>
2017-10-24 03:01:43 +0200

    Added basic fuel for sale view.

---
+++ source/UI/InjectedComps/CommsRemoteTradeComp.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace WM.SelfLaunchingPods
+{
+	public class CommsRemoteTradeCompProperties : CompProperties
+	{
+		public CommsRemoteTradeCompProperties()
+		{
+			this.compClass = typeof(CommsRemoteTradeComp);
+		}
+	}
+
+	public class CommsRemoteTradeComp : ThingComp
+	{
+		public Building_CommsConsole Parent
+		{
+			get
+			{
+				return (this.parent as Building_CommsConsole);
+			}
+		}
+
+		//TODO: can't figure out why this is not working. Using detour.
+		public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn)
+		{
+			if (!Parent.CanUseCommsNow)
+				return (null);
+
+			var travelers = TradeUtils.GetRemoteTradeable();
+
+			if (!travelers.Any())
+				return (null);
+
+			var extraoptions = GetRemoteTradingMenuOptions(selPawn, travelers);
+
+			return (extraoptions);
+		}
+
+		IEnumerable<FloatMenuOption> GetRemoteTradingMenuOptions(Pawn myPawn, IEnumerable<WorldTraveler> travelers)
+		{
+			foreach (var item in travelers)
+			{
+				var option = new FloatMenuOption(MenuOptionPriority.High);
+
+				option.Label = item.remoteTrader.GetCallLabel();
+				option.action = delegate
+				{
+					var job = new Job(JobDefOf.UseCommsConsole, Parent);
+
+					job.commTarget = item.remoteTrader;
+					myPawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+				};
+				yield return (option);
+			}
+		}
+	}
+}
---
+++ source/UI/InjectedComps/FactionBaseAvailableFuelComp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace WM.SelfLaunchingPods
+{
+	public class FactionBaseAvailableFuelCompProperties : WorldObjectCompProperties
+	{
+		public FactionBaseAvailableFuelCompProperties()
+		{
+			this.compClass = typeof(FactionBaseAvailableFuelComp);
+		}	}
+
+	public class FactionBaseAvailableFuelComp : WorldObjectComp
+	{
+		public override string CompInspectStringExtra()
+		{
+			var factionBase = this.parent as FactionBase;
+
+			if (factionBase == null || factionBase.trader == null || !factionBase.trader.CanTradeNow)
+				return "";
+
+			var fuelQt = factionBase.trader.StockListForReading
+									.Where((Thing arg) => arg.def == DefOf.Chemfuel)
+									.Sum((Thing arg) => arg.stackCount);
+
+			if (fuelQt <= 0)
+				return "";
+
+			return (string.Format("WM.FactionBaseInspectStringFuelForSale".Translate(), fuelQt, DefOf.Chemfuel.label));
+		}
+	}
+}
